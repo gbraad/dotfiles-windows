fdae9a6426a266c89600b537f33277a9e24f17fb
Gerard Braad <me@gbraad.nl>
2023-06-08 12:31:01 +0800

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -19,3 +19,8 @@ function OnViModeChange {
 #Set-PSReadLineOption -ViModeIndicator Script -ViModeChangeHandler $Function:OnViModeChange
 
 Set-PSReadLineOption -EditMode vi
+
+# Aliases
+function devsys  { podman run -d --name=devsys --hostname podman-devsys --systemd=always --cap-add=NET_ADMIN --cap-add=NET_RAW --device=/dev/net/tun -v C:\Users\gbraad\Projects:/home/gbraad/Projects ghcr.io/gbraad-devenv/fedora/systemd:38 }
+function devroot { podman exec -it devsys zsh }
+function devuser { podman exec -it devsys su - gbraad }
